cebaa5512f9343d97934b5422bf2c991b560f3de
Kassem Johami <108426720+DevJohami@users.noreply.github.com>
2022-08-14 09:49:44 -0400

    Create notes.sh

---
+++ notes.sh
@@ -0,0 +1,12 @@
+# This is how you would echo using the \ character
+
+# name='Rita'
+
+# We would like to display the following string using echo using the escape character \
+
+# How is Rita's husband?
+
+# Write your solution in the textbox given below. Make sure there are no unnecessary spaces, i.e. no space before and after the entire sentence and only one space between the words.
+
+name='Rita'
+echo How is $name\'s husband?
